a08157a14125f771090bcd3468448aff54ea1faf
MarcoPanz <marcopanzan23@gmail.com>
2024-06-11 19:51:56 +0200

    counter finito

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -8,9 +8,9 @@ class Counter extends Component
 {
     public $count = 1;
 
-    public function increment()
+    public function increment($n = 1)
     {
-        $this->count++;
+        $this->count += $n;
     }
 
     public function decrement()
@@ -20,5 +20,9 @@ class Counter extends Component
     public function render()
     {
         return view('livewire.counter');
+    }
+    public function resetCounter()
+    {
+        $this->count = 0;
     }
 }
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,10 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <h1>{{ $count }}</h1>
+
+    <button wire:click="increment">+</button>
+
+    <button wire:click="decrement">-</button>
+    <button wire:click="increment(25)">+25</button>
+    <button wire:click="resetCounter">reset</button>
+
 </div>
